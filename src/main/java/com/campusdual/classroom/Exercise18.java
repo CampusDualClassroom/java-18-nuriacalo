cb5091fb2f3a706542aee482670fedfea09dc344
Nuria Calo <nuriacalomosquera@gmail.com>
2025-05-26 12:12:19 +0200

    exercice 18 change

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -9,15 +9,19 @@ public class Exercise18 {
     public static int[] createAndInitializeArray(int num) {
         int[] numberArray = new int[num];
         for (int i = 0; i < num; i++) {
-            numberArray[i] = Utils.integer("Enter a number: ");
+            numberArray[i] = i + 1;
         }
         return numberArray;
     }
 
     public static void showInlineArray(int[] lineaArray) {
         for (int i = 0; i < lineaArray.length; i++) {
-            System.out.println(lineaArray[i]);
+            System.out.print(lineaArray[i]);
+            if (i < lineaArray.length - 1) {
+                System.out.print(" ");
+            }
         }
+        System.out.println();
     }
 
     public static void main(String[] args) {
